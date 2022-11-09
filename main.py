daba9c6696a3621a4df76e5c0c6dac6cb8f52966
Russell Johnson <blueyorange@gmail.com>
2022-11-09 16:00:08 +0000

    converts page to markdown file

--- main.py
+++ main.py
@@ -7,10 +7,28 @@ import pdf2image
 import re
 
 TEXT_BOX_MIN_ASPECT_RATIO = 3
-START_PAGE = 4
-END_PAGE = 4
-DILATION_KERNEL = (3,4)
-CHARACTER_AREA = DILATION_KERNEL[0]*DILATION_KERNEL[1]
+START_PAGE = 2
+END_PAGE = 2
+DILATION_KERNEL = (3, 5)
+
+
+class Item:
+    def __init__(self, image, text):
+        self.img = image
+        self.txt = text
+
+
+class Question:
+    items = []
+    options = {}
+
+    def addItem(self, image):
+        self.items.append(Item(image))
+
+    def addOption(self, key, value):
+        # key should be A, B, C, or D
+        self.options[key] = value
+
 
 def rejectChildBoxes(boxes):
     """Takes a list of box tuples in form x,y,w,h and rejects those inside another"""
@@ -26,46 +44,60 @@ def rejectChildBoxes(boxes):
             filtered.append(box2)
     return filtered
 
+
 def getROIsFromImage(mat):
     mat = cv.threshold(mat, 127, 255, cv.THRESH_BINARY)[1]
     mat = cv.Canny(mat, 200, 255)
     mat = cv.dilate(mat, np.ones(
-            DILATION_KERNEL, np.uint8), iterations=5)
+        DILATION_KERNEL, np.uint8), iterations=5)
     contours = cv.findContours(mat, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     contours = contours[0] if len(contours) == 2 else contours[1]
     boxes = [cv.boundingRect(c) for c in contours]
-    boxes= rejectChildBoxes(boxes)
+    boxes = rejectChildBoxes(boxes)
     return boxes
+
 
 def drawBoxes(img, boxes):
     img = img.copy()
     for box in boxes:
-        x,y,w,h = box
-        cv.rectangle(img, (x,y),(x+w, y+h), 2)
+        x, y, w, h = box
+        cv.rectangle(img, (x, y), (x+w, y+h), 2)
     return img
 
-def imageToString(img, psm=6):
-    return pytesseract.image_to_string(img, config=f'--psm {psm}')
+
+def imageToString(img):
+    return pytesseract.image_to_string(img, config=f'--psm 6')
+
 
 def sortBoxesTopToBottom(boxes):
     boxes = boxes.copy()
     boxes.sort(key=lambda b: b[1])
     return boxes
 
+
 def sortBoxesLeftToRight(boxes):
     boxes = boxes.copy()
     boxes.sort(key=lambda b: b[0])
     return boxes
 
+
+def sortBoxes(boxes):
+    boxes = boxes.copy()
+    boxes.sort(key=lambda b: b[0]+b[1])
+    return boxes
+
+
 def isTextBoxByAspectRatio(img):
-    h,w,_ = img.shape
+    h, w, _ = img.shape
     return w/h > TEXT_BOX_MIN_ASPECT_RATIO
+
 
 def convertToImageOrText(img):
     if isTextBoxByAspectRatio(img):
         return imageToString(img)
     else:
         return img
+
 
 def matrixToImage(mat):
     return Image.fromarray(mat)
@@ -75,43 +107,73 @@ def matrixToImage(mat):
     # imageBoxes = [box for box in boxes if not isTextBoxByAspectRatioFunc(
     #     TEXT_BOX_MIN_ASPECT_RATIO)(box)]
 
-def cropROI(mat):
+
+def createCropper(mat):
     def fn(box):
-        x,y,w,h = box
-        return mat [y:y+h,x:x+w]
+        x, y, w, h = box
+        return mat[y:y+h, x:x+w]
     return fn
 
-def getQuestionNumberBoxes(mat, boxes):
-    sorted = sortBoxesLeftToRight(boxes)
-    questionNumberBoxes = []
-    for box in sorted:
-        cropper = cropROI(mat)
-        crop = cropper(box)
-        text = imageToString(crop, psm=10).strip()
-        if re.match('^\d+$', text):
-            questionNumberBoxes.append(box)
-        else: 
-            return questionNumberBoxes
+
+def nestBoxesByLine(boxes):
+    sortedVertically = sortBoxesTopToBottom(boxes)
+    nestedList = []
+    currentLine = []
+    for i in range(len(boxes)-1):
+        print(nestedList)
+        currentLine.append(boxes[i])
+        y1 = boxes[i][1]
+        h1 = boxes[i][3]
+        y2 = boxes[i+1][1]
+        h2 = boxes[i+1][3]
+        if y2 > (y1+h1) or y1 > (y2+h2):
+            print("newline")
+            currentLine = sortBoxesLeftToRight(currentLine)
+            nestedList.append(currentLine)
+            currentLine = []
+        else:
+            print("same line")
+    return nestedList
+
 
 def processPage(page):
     mat = np.asarray(page)
     boxes = getROIsFromImage(mat)
-    questionNumberBoxes = getQuestionNumberBoxes(mat, boxes)
-    crop = cropROI(mat)
-    pageWithROIBoxes = drawBoxes(mat, boxes)
-    crops = map(cropROI(mat), boxes)
     boxes = sortBoxesTopToBottom(boxes)
-    outputStr=""
-    cv.imshow('new', pageWithROIBoxes)
-    for crop in crops:
-        outputStr = outputStr + imageToString(crop)
-        print(outputStr)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    lines = nestBoxesByLine(boxes)
+    newQuestionRe = '^\d+$'
+    endQuestionRe = 'Your answer'
+    optionLetterRe = '[ABCD]'
+    questions = []
+    capture = False
+    optionLetter = ""
+    # remove first line (page number)
+    output = []
+    for boxes in lines:
+        items = []
+        for box in boxes:
+            x, y, w, h = box
+            crop = mat[y:y+h, x:x+w]
+            text = imageToString(crop)
+            item = Item(crop, text)
+            items.append(item)
+        output.append(items)
+    return output
     exit()
-    
+
+
 if __name__ == "__main__":
     pages = pdf2image.convert_from_path('testpdf.pdf')
-    pages = pages[START_PAGE-1:END_PAGE+1]
-    page = pages[-1]
-    processPage(page)
+    pages = pages[START_PAGE-1:END_PAGE]
+    page = pages[0]
+    output = processPage(page)
+    n = 0
+    with open('test.md', 'a') as f:
+        for line in output:
+            for item in line:
+                n += 1
+                img = Image.fromarray(item.img)
+                imgPath = f'./Images/Image{n}.png'
+                img.save(imgPath)
+                f.write(f'![{item.txt}]({imgPath}) ')
+            f.write('\n\n')
---
+++ test.md
@@ -0,0 +1,82 @@
+![2
+](./Images/Image1.png) 
+
+![SECTION A
+](./Images/Image2.png) 
+
+![You should spend a maximum of 25 minutes on this section.
+](./Images/Image3.png) 
+
+![Answer all the questions.
+](./Images/Image4.png) 
+
+![Write your answer to each question in the box provided.
+](./Images/Image5.png) 
+
+![1
+](./Images/Image6.png) ![The watt is the SI unit for power.
+](./Images/Image7.png) 
+
+![Which is the correct definition for the watt? A watt is ....
+](./Images/Image8.png) 
+
+![A
+](./Images/Image9.png) ![the rate of work done.
+](./Images/Image10.png) 
+
+![B
+](./Images/Image11.png) ![the work done per second.
+](./Images/Image12.png) 
+
+![Cc
+](./Images/Image13.png) ![a joule per second.
+](./Images/Image14.png) 
+
+![D
+](./Images/Image15.png) ![a joule per unit time.
+](./Images/Image16.png) 
+
+![Your answer
+](./Images/Image17.png) ![[|
+](./Images/Image18.png) ![11]
+](./Images/Image19.png) 
+
+![2
+](./Images/Image20.png) ![A crane is used to lift a load directly from point X to point Y.
+](./Images/Image21.png) 
+
+![p I P
+w fq
+Xx 'Z
+r
+w
+](./Images/Image22.png) 
+
+![The weight of the load is W.
+Pp, gand rare distances between points X, Y and Z as shown in the diagram.
+](./Images/Image23.png) 
+
+![What is the work done against the weight?
+](./Images/Image24.png) 
+
+![A
+](./Images/Image25.png) ![Wp
+](./Images/Image26.png) 
+
+![B
+](./Images/Image27.png) ![Wa
+](./Images/Image28.png) 
+
+![Cc
+](./Images/Image29.png) ![Wr
+](./Images/Image30.png) 
+
+![D
+](./Images/Image31.png) ![Wq+r)
+](./Images/Image32.png) 
+
+![Your answer
+](./Images/Image33.png) ![[|
+](./Images/Image34.png) ![11]
+](./Images/Image35.png) 
+
